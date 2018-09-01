4ef5fd0a16e2ec585b763532d83935116b063fc8
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-01 14:47:14 +0900

    refactor:置けるかチェックを処理を短縮

--- bord.rb
+++ bord.rb
@@ -24,11 +24,61 @@ class Bord
     end
   end
 
+  def put_locate_check(stone_no)
+    @my_stone = stone_no
+    array = []
+    right_check("i","j + 1")
+    left_check("i","j - 1")
+    up_check("i - 1","j")
+    down_check("i + 1","j")
+    right_down_check("i + 1","j + 1")
+    right_up_check("i - 1","j + 1")
+    left_down_check("i + 1","j - 1")
+    left_up_check("i - 1","j - 1")
+
+    for i in 0..@put_list.count - 1
+      puts i.to_s + "|" + @put_list[i]
+      @locate_list.push(@put_list[i])
+    end
+
+  end
+
+  def method_missing(method,*args)
+    unless method !~ /.*check/
+      check(args[0],args[1])
+    end
+  end
+
+  def check(x,y)
+    a = <<-LOOP
+    for i in 1..@ban[0].count - 1
+      for j in 1..@ban[0].count - 1
+        if @ban[i][j] == @my_stone && !(@ban[#{x}][#{y}] == @my_stone) && !(@ban[#{x}][#{y}] == 0)
+          until @ban[#{x}][#{y}] == 9
+            if @ban[#{x}][#{y}] == 0
+              @put_list.push((#{x}).to_s + (#{y}).to_s)
+              break
+            end
+            i = #{x}
+            j = #{y}
+          end
+        end
+      end
+    end
+    LOOP
+    eval(a)
+  end
+
   def put_stone(stone_no,input)
     reverse_stone(@locate_list[input][0],@locate_list[input][1])
     # right_reverse
     # left_reverse
 
+  end
+
+  def reverse_stone(x,y)
+    @tate = x.to_i
+    @yoko = y.to_i
   end
 
   # def right_reverse
@@ -62,55 +112,5 @@ class Bord
   #       end
   #     end
   # end
-
-  def put_locate_check(stone_no)
-    @my_stone = stone_no
-    array = []
-    right_check("i","j + 1")
-    left_check("i","j - 1")
-    up_check("i - 1","j")
-    down_check("i + 1","j")
-    right_down_check("i + 1","j + 1")
-    right_up_check("i - 1","j + 1")
-    left_down_check("i + 1","j - 1")
-    left_up_check("i - 1","j - 1")
-
-    for i in 0..@put_list.count - 1
-      puts i.to_s + "|" + @put_list[i]
-      @locate_list.push(@put_list[i])
-    end
-
-  end
-
-  def reverse_stone(x,y)
-    @tate = x.to_i
-    @yoko = y.to_i
-  end
-
-  def method_missing(method,*args)
-    unless method !~ /.*check/
-      check(args[0],args[1])
-    end
-  end
-
-  def check(x,y)
-    a = <<-LOOP
-    for i in 1..@ban[0].count - 1
-      for j in 1..@ban[0].count - 1
-        if @ban[i][j] == @my_stone && !(@ban[#{x}][#{y}] == @my_stone) && !(@ban[#{x}][#{y}] == 0)
-          until @ban[#{x}][#{y}] == 9
-            if @ban[#{x}][#{y}] == 0
-              @put_list.push((#{x}).to_s + (#{y}).to_s)
-              break
-            end
-            i = #{x}
-            j = #{y}
-          end
-        end
-      end
-    end
-    LOOP
-    eval(a)
-  end
 
 end
